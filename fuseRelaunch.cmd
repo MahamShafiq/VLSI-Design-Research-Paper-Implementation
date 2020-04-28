df7ce48323d773549d3819b747cfaa9de0757935
Maham Shafiq <43970527+MahamShafiq@users.noreply.github.com>
2020-04-29 01:41:14 +0500

    Add files via upload

---
+++ fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "simprims_ver" -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/Users/maham/Desktop/My semester/vlsi/CEP/testwhenexternalmemoryishere_isim_par.exe" -prj "C:/Users/maham/Desktop/My semester/vlsi/CEP/testwhenexternalmemoryishere_par.prj" "work.testwhenexternalmemoryishere" "work.glbl" 
